61efd1669ea6cb3c2112bbb7a33b4ca99526008c
Diana <73073704+VidyakinaDiana@users.noreply.github.com>
2021-11-01 01:20:41 +0300

    Add files via upload

---
+++ Mirror.h
@@ -0,0 +1,19 @@
+#ifndef laba17
+#define laba 17
+template <typename T>
+T* mirror(T* mass, int size) {
+	try {
+		if (size % 2 == 1 || size == 0)
+			throw - 1;
+		T* temp = new T[size];
+		for (int i = 0; i < size; i++)
+			temp[i] = mass[size - i - 1];
+		mass = temp;
+		return mass;
+	}
+	catch (int n) {
+		std::cout << "incorrect size" << std::endl;
+		return 0;
+	}
+}
+#endif laba17
