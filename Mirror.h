dfa7e06045ab3a636c4737665a9ba6dd12cf5752
Diana <73073704+VidyakinaDiana@users.noreply.github.com>
2021-11-01 01:47:41 +0300

    Add files via upload

--- Mirror.h
+++ Mirror.h
@@ -2,18 +2,12 @@
 #define laba 17
 template <typename T>
 T* mirror(T* mass, int size) {
-	try {
-		if (size % 2 == 1 || size == 0)
-			throw - 1;
-		T* temp = new T[size];
-		for (int i = 0; i < size; i++)
-			temp[i] = mass[size - i - 1];
-		mass = temp;
-		return mass;
-	}
-	catch (int n) {
-		std::cout << "incorrect size" << std::endl;
-		return 0;
-	}
+	if (size % 2 == 1 || size == 0)
+		throw "incorrect size";
+	T* temp = new T[size];
+	for (int i = 0; i < size; i++)
+		temp[i] = mass[size - i - 1];
+	mass = temp;
+	return mass;
 }
 #endif laba17
--- mirrormaincpp.cpp
+++ mirrormaincpp.cpp
@@ -2,38 +2,24 @@
 #include <time.h>
 #include "Mirror.h"
 int main() {
-	srand(time(NULL));
-	int size;
-	std::cin >> size;
-	while(size<0)
+	try {
+		srand(time(NULL));
+		int size;
 		std::cin >> size;
-	int* mas = new int[size];
-	for (int i = 0; i < size; i++)
-		std::cout << (mas[i] = rand() / 20) << " ";
-	std::cout << std::endl;
-	mas = mirror(mas, size);
-	for (int i = 0; i < size; i++)
-		std::cout << mas[i] << " ";
-	std::cout << std::endl;
-	std::cin >> size;
-	while (size < 0)
-		std::cin >> size;
-	char* mas1 = new char[size];
-	for (int i = 0; i < size; i++)
-		std::cout << (mas1[i] = (char)(rand() / 20)) << " ";
-	std::cout << std::endl;
-	mas1 = mirror(mas1, size);
-	for (int i = 0; i < size; i++)
-		std::cout << mas1[i] << " ";
-	std::cout << std::endl;
-	size = 5;
-	int* mas2 = new int[size];
-	for (int i = 0; i < size; i++)
-		std::cout << (mas2[i] = rand() / 20) << " ";
-	std::cout << std::endl;
-	mas2 = mirror(mas2, size);
-	delete[] mas;
-	delete[] mas1;
-	delete[] mas2;
+		while (size < 0)
+			std::cin >> size;
+		char* mas = new char[size];
+		for (int i = 0; i < size; i++)
+			std::cout << (mas[i] = (char)(3 + rand() % 120)) << " ";
+		std::cout << std::endl;
+		mas = mirror(mas, size);
+		for (int i = 0; i < size; i++)
+			std::cout << mas[i] << " ";
+		std::cout << std::endl;
+		delete[] mas;
+	}
+	catch (...) {
+		std::cout << " Incorrect number ";
+	}
 	return 0;
 }
